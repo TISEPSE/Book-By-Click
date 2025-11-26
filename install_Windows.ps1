3d3fd4d19b3d885e96e662a7b68daf8944791fbb
TISEPSE <titi21player@gmail.com>
2025-11-26 12:00:54 +0100

    modif

---
+++ install_Windows.ps1
@@ -0,0 +1,11 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"if (!(Test-Path 'venv')) { ^
+    Write-Host 'Création du virtualenv...'; ^
+    python -m venv venv ^
+} ; ^
+. venv/Scripts/Activate.ps1 ; ^
+pip install --upgrade pip ; ^
+pip install flask ; ^
+npm install ; ^
+npm run api"
