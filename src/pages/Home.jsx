3cffdb6bfa1a50598390ba5f239bacc928a854df
TISEPSE <titi21player@gmail.com>
2025-11-29 00:36:22 +0100

    Modiff hero banner, crée et se connecter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -51,16 +51,16 @@ export default function Home() {
             </p>
             <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
               <a
-                href="#"
+                href="login_form"
                 className="block py-2 px-4 text-center text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none"
               >
-                Let's get started
+                Se connecter
               </a>
               <a
-                href="#"
+                href="register_form"
                 className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex"
               >
-                Get access
+                Crée un compte
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
