16e7e3a829a2262178464a0ce4cf12d112466e81
TISEPSE <titi21player@gmail.com>
2025-11-28 17:17:27 +0100

    Login fonctionelle

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/login", methods=["POST"])
+@app.route("/login_form", methods=["POST"])
 def login_Form():
-    
-    email = request.form.get("name")
+    email = request.form.get("email")
     password = request.form.get("password")
+    print(f"Email: {email}, Password: {password}", flush=True)
+    return "OK"
 
-    print(f"Email: {email}, Password: {password}")
+
+if __name__ == "__main__":
+    app.run(port=5000)
