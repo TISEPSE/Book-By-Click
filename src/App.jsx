31f8f5cd1215ba920e31a04145ccef6be39a3d52
TISEPSE <titi21player@gmail.com>
2025-12-02 19:33:44 +0100

    ajout dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Resgister_user from './pages/Register_User.jsx';
 import Resgister_pro from './pages/Register_Pro.jsx';
 import Choice_Register from "./pages/Choice_Register.jsx"
 import Forgot_password from "./pages/Forgot_password.jsx"
+import Dashboard_Entreprise from "./pages/Dashboard_Entreprise.jsx"
 import "./static/App.css";
 //===============PAGE DE TEST=================
 import Test from "./pages/Teste_form.jsx"
@@ -22,6 +23,7 @@ function App() {
         <Route path="/register_form_user" element={<Resgister_user/>}/>
         <Route path="/register_form_pro" element={<Resgister_pro/>}/>
         <Route path="/fogot_password" element={<Forgot_password/>}/>
+        <Route path="/dashboard_entreprise" element={<Dashboard_Entreprise/>}/>
         
         <Route path="/teste" element={<Test/>} />
         <Route path="/recap" element={<Recap/>}/>
