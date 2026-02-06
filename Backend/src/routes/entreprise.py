cfb30a1404d345bbbbba34b1369f21cfdc09a9ed
bababooey-y <maximegeffroy24@gmail.com>
2026-02-06 11:21:56 +0100

    recherche d'entreprises /api/entreprise/search

---
+++ Backend/src/routes/entreprise.py
@@ -0,0 +1,70 @@
+from flask import Flask, Blueprint, request, jsonify
+from src.extension import db
+
+
+
+
+
+
+entreprise_blueprint = Blueprint("entreprise",__name__)
+
+
+@entreprise_blueprint.route("/api/entreprise/search", methods=["POST"])
+def search():
+    data = request.get_json()
+    service_entreprise = data.get("service")
+    localisation = data.get("localisation")
+
+    if not service_entreprise and not localisation:
+        return jsonify({"error": "No data"}),400
+    
+    if service_entreprise and localisation:
+        entreprises = Entreprise.query.filter(Entreprise.ville.like(f"%{localisation}%")).filter(Entreprise.service.like(f"%{service}%")).all()
+
+        ret_dict = {}
+        for entreprise in entreprises:
+            ret_dict[entreprise.nomEntreprise] = {
+                "secteur" : entreprise.nomSecteur,
+                 "slug" : entreprise.slugPublic,
+                "adresse" : entreprise.adresse,
+                "code_postal" : entreprise.codePostal,
+                "ville": entreprise.ville,
+                "pays": entreprise.pays
+                
+                }
+        return jsonify(ret_dict), 203
+
+    
+    if not service_entreprise:
+        entreprises = Entreprise.query.filter(Entreprise.ville.like(f'%{localisation}%')).all()
+        ret_dict = {}
+        for entreprise in entreprises:
+            ret_dict[entreprise.nomEntreprise] = {
+                "secteur" : entreprise.nomSecteur,
+                 "slug" : entreprise.slugPublic,
+                "adresse" : entreprise.adresse,
+                "code_postal" : entreprise.codePostal,
+                "ville": entreprise.ville,
+                "pays": entreprise.pays
+                
+                }
+        return jsonify(ret_dict), 203
+
+    if not localisation:
+        entreprises = Entreprise.query.filter(Entreprise.ville.like(f'%{service_entreprise}%')).all()
+        ret_dict = {}
+        for entreprise in entreprises:
+            ret_dict[entreprise.nomEntreprise] = {
+                "secteur" : entreprise.nomSecteur,
+                 "slug" : entreprise.slugPublic,
+                "adresse" : entreprise.adresse,
+                "code_postal" : entreprise.codePostal,
+                "ville": entreprise.ville,
+                "pays": entreprise.pays
+                
+                }
+        return jsonify(ret_dict), 203
+    return jsonify({"error": "error server"}), 500
+
+
+
